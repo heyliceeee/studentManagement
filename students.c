8857332d3b4d20a28ffe081905da18d2b5d8d172
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-12-05 16:55:45 +0000

    :sparkles: feat: remove student data

--- students.c
+++ students.c
@@ -19,3 +19,16 @@ void printStudent(Student student)
     printf("\n%3d %-30s %d-%d-%d", student.number, student.name, student.birth.day, student.birth.month, student.birth.year);
 }
 
+
+/**
+ * remove student data
+ * @param student removed
+ */
+void removeStudentData(Student *student)
+{
+    (*student).number = 0;
+
+    strcpy((*student).name, "");
+
+    (*student).birth.day = (*student).birth.month = (*student).birth.year = 0;
+}
