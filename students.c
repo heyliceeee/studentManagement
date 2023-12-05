b6c100482d6a471760ceea170043b072c69ed9a2
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-12-05 16:48:43 +0000

    :sparkles: feat: print student

--- students.c
+++ students.c
@@ -2,4 +2,20 @@
 // Created by Alice Dias on 12/5/2023.
 //
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "students.h"
+#include "input.h"
+
+
+/**
+ * print one student
+ * @param student printed
+ */
+void printStudent(Student student)
+{
+    printf("\n%3d %-30s %d-%d-%d", student.number, student.name, student.birth.day, student.birth.month, student.birth.year);
+}
+
